92bd561045c7da5160fbddcdbe6d433ba9e1a8ee
Bruno Martucci <brunomartucci77@gmail.com>
2023-12-07 17:27:34 -0200

    Movimentação de peças

--- jogo-de-xadrez/src/boardgame/Board.java
+++ jogo-de-xadrez/src/boardgame/Board.java
@@ -44,6 +44,19 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Essa posição nao existe!");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 
 	private boolean positionExists(int row, int colunm) {
 		return row >= 0 && row < rows && colunm >= 0 && colunm < columns;
